f0dc35993b8f4fc78076196273480a1c4e06f123
Santiago Enciso <whereispolaris@gmail.com>
2019-11-11 19:48:53 -0600

    Added basic React component code to Single and PhotoGrid components.

---
+++ client/components/PhotoGrid.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const PhotoGrid = React.createClass({
+    render() {
+        return (
+            <div className="single-grid">
+                I'm the Photo Grid
+            </div>
+        )
+    }
+})
+
+export default PhotoGrid;
---
+++ client/components/Single.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Single = React.createClass({
+    render() {
+        return (
+            <div className="single-grid">
+                I'm the Single Grid
+            </div>
+        )
+    }
+})
+
+export default Single;
