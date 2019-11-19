0611da8a3a96820220a837dcb1f2797f088436c1
Santiago Enciso <whereispolaris@gmail.com>
2019-11-19 12:45:35 -0600

    Created Redux store file and imported dependencies.

---
+++ client/store.js
@@ -0,0 +1,9 @@
+import { createStore, compse } from 'redux';
+import { syncHistoryWithStore } from 'react-router-redux';
+import { browserHistory } from 'react-router';
+
+// import root reducer
+import rootReducer from './reducers/index';
+
+import comments from './data/comments';
+import posts from './data/posts';
