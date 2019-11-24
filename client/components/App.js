29f3a38044ecb421d198a7820e945f6b61281db6
Santiago Enciso <whereispolaris@gmail.com>
2019-11-24 12:37:58 -0600

    Fixed bugs in App file.

--- client/components/App.js
+++ client/components/App.js
@@ -1,19 +1,19 @@
-import { bindActionCreators } from "redux"
+import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import * as actionCreators from '../actions/actionCreators';
 import Main from './Main';
 
 function mapStateToProps(state) {
     return {
-        posts: state.props,
+        posts: state.posts,
         comments: state.comments
     }
 }
 
-function mapDispatchToProps(dispatch) {
+function mapDispachToProps(dispatch) {
     return bindActionCreators(actionCreators, dispatch);
 }
 
-const App = connect(mapStateToProps, mapDispatchToProps)(Main);
+const App = connect(mapStateToProps, mapDispachToProps)(Main);
 
 export default App;
