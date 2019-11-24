4ed18170c39fafd8388651e28c8422d3d0f5e37a
Santiago Enciso <whereispolaris@gmail.com>
2019-11-24 11:37:58 -0600

    Created App component with connect method. This makes the state and actions available for any component we import App to.

---
+++ client/components/App.js
@@ -0,0 +1,19 @@
+import { bindActionCreators } from "redux"
+import { connect } from 'react-redux';
+import * as actionCreators from '../actions/actionCreators';
+import Main from './Main';
+
+function mapStateToProps(state) {
+    return {
+        posts: state.props,
+        comments: state.comments
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators(actionCreators, dispatch);
+}
+
+const App = connect(mapStateToProps, mapDispatchToProps);
+
+export default App;
