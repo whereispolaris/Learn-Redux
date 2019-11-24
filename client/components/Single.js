f9335a1bfb52ff8f854f16a1c17b15e451b1e9b8
Santiago Enciso <whereispolaris@gmail.com>
2019-11-24 12:59:07 -0600

    Updated className from single-grid to single-photo.

--- client/components/Single.js
+++ client/components/Single.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Single = React.createClass({
     render() {
         return (
-            <div className="single-grid">
+            <div className="single-photo">
                 I'm the Single Grid
             </div>
         )
