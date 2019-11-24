c07d031470feaa46ff88de8b4d52b03d3713f4bd
Santiago Enciso <whereispolaris@gmail.com>
2019-11-24 12:56:43 -0600

    Added div with post code props and caption for each result.

--- client/components/Photo.js
+++ client/components/Photo.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const Photo = React.createClass({
     render() {
+        const { post, i, comments } = this.props;
         return (
             <figure className="grid-figure">
-                I am a photo!
+                <div className="grid-photo-wrap">
+                    <Link to={`/view/${post.code}`}>
+                        {post.caption}
+                    </Link>
+                </div>
             </figure>
         )
     }
