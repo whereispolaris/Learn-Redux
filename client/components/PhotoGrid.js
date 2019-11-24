a9c2755195aec93a3dee5f2ee5cc776a35336e97
Santiago Enciso <whereispolaris@gmail.com>
2019-11-24 12:51:31 -0600

    Added props to photo element in map method.

--- client/components/PhotoGrid.js
+++ client/components/PhotoGrid.js
@@ -4,9 +4,9 @@ import Photo from './Photo';
 const PhotoGrid = React.createClass({
     render() {
         return (
-            <div className="single-grid">
+            <div className="photo-grid">
                 {this.props.posts.map(
-                    (post, i) => <Photo />
+                    (post, i) => <Photo {...this.props} key={i} i={i} post={post} />
                 )}
             </div>
         )
