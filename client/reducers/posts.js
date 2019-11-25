02de85b3a0066b9b129822e089ac546bcfc1d96c
Santiago Enciso <whereispolaris@gmail.com>
2019-11-25 17:46:27 -0600

    Added switch statement to posts function that will find the photo by index and add +1 to the likes unsing spread and slice.

--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -3,9 +3,20 @@
 // 2. copy of the current state
 
 function posts(state = [], action) {
-    console.log("The post will change");
-    console.log(state, action);
-    return state;
+    switch (action.type) {
+        case 'INCREMENT_LIKES':
+            console.log('Incrementing likes!!!');
+            const i = action.index;
+            return [
+                ...state.slice(0, i), // before the one we
+                { ...state[i], likes: state[i].likes + 1 },
+                ...state.slice(i + 1), //after the one we're updating
+
+            ]
+        default:
+            return state;
+    }
+
 }
 
 export default posts;
