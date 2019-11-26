57aed2a86f2c83c6e1b1aad15dff8ed3c755c83b
Santiago Enciso <whereispolaris@gmail.com>
2019-11-25 18:16:47 -0600

    Created Comment component.

---
+++ client/components/Comments.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Comments = React.createClass({
+    render() {
+        <div className="comment">
+            I'm the comment!
+        </div>
+    }
+});
+
+export default Comments;
