470cb8c8f11af8d4e9fd9bbe0c3520fb86c76705
Santiago Enciso <whereispolaris@gmail.com>
2019-11-25 18:54:21 -0600

    Destructured comments from props, created postComments variable that grabs the comments based on the post ID, passed postComments as prop to Comments component.

--- client/components/Single.js
+++ client/components/Single.js
@@ -5,11 +5,12 @@ import Comments from './Comments';
 
 const Single = React.createClass({
     render() {
-        const { posts, params } = this.props
+        const { posts, params, comments } = this.props
         // index of the post
         const i = posts.findIndex((post) => post.code === params.postId)
         // get us the post
         const post = posts[i];
+        const postComments = comments[params.postId] || [];
         return (
             <div className="single-photo">
                 <Photo
@@ -17,7 +18,7 @@ const Single = React.createClass({
                     key={i}
                     i={i}
                     post={post} />
-                <Comments />
+                <Comments postComments={postComments} />
             </div>
         )
     }
