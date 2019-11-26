4280513a7f2afd2ef793c26d680b4485b92ccd70
Santiago Enciso <whereispolaris@gmail.com>
2019-11-25 19:07:01 -0600

    Added comment form to Comment component.

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Comments = React.createClass({
     renderComment(comment, i) {
         return (
-            <div className="comment">
+            <div className="comment" key={i}>
                 <p>
                     <strong>{comment.user}</strong>
                     {comment.text}
@@ -16,6 +16,11 @@ const Comments = React.createClass({
         return (
             <div className="comments">
                 {this.props.postComments.map(this.renderComment)}
+                <form ref="commentForm" className="comment-form">
+                    <input type="text" ref="author" placeholder="author" />
+                    <input type="text" ref="comment" placeholder="comment" />
+                    <input type="submit" hidden />
+                </form>
             </div>
         )
     }
