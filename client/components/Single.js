d6316b7bd28993a45a6cfc8366b24fcc0594ec51
Santiago Enciso <whereispolaris@gmail.com>
2019-11-25 18:13:19 -0600

    Removed console.logs

--- client/components/Single.js
+++ client/components/Single.js
@@ -8,10 +8,8 @@ const Single = React.createClass({
         const { posts, params } = this.props
         // index of the post
         const i = posts.findIndex((post) => post.code === params.postId)
-        console.log(i);
         // get us the post
         const post = posts[i];
-        console.log(post);
         return (
             <div className="single-photo">
                 <Photo
