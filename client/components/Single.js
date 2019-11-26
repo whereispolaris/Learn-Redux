fd5a32b3aff8b91b0919b12f7684a06d53b51df8
Santiago Enciso <whereispolaris@gmail.com>
2019-11-25 18:04:24 -0600

    imported the Photo component, created variable that grabs the index of the post and logs it to the console.

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import Photo from './Photo';
+
+// import Comments
 
 const Single = React.createClass({
     render() {
+        // index of the post
+        const i = this.props.posts.findIndex((post) => post.code === this.props.params.postId)
+        console.log(i);
+        // get us the post
         return (
             <div className="single-photo">
                 I'm the Single Grid
