3c1c9970be97b88684849755da80adab970fe40e
Santiago Enciso <whereispolaris@gmail.com>
2019-11-25 18:59:34 -0600

    Created function that renders comments to Single post page.

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -1,10 +1,20 @@
 import React from 'react';
 
 const Comments = React.createClass({
+    renderComment(comment, i) {
+        return (
+            <div className="comment">
+                <p>
+                    <strong>{comment.user}</strong>
+                    {comment.text}
+                </p>
+            </div>
+        )
+    },
     render() {
         return (
             <div className="comment">
-                I'm the comment!
+                {this.props.postComments.map(this.renderComment)}
             </div>
         )
     }
