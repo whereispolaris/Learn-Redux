71ac19d37652bf69e7418647a15ea203f7bdd9a0
Santiago Enciso <whereispolaris@gmail.com>
2019-11-27 12:47:53 -0600

    Fixed bug in onclick event.

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -19,7 +19,7 @@ const Comments = React.createClass({
         return (
             <div className="comments">
                 {this.props.postComments.map(this.renderComment)}
-                <form ref="commentForm" className="comment-form" onSubmit={this.handeSumit}>
+                <form ref="commentForm" className="comment-form" onSubmit={this.handleSubmit}>
                     <input type="text" ref="author" placeholder="author" />
                     <input type="text" ref="comment" placeholder="comment" />
                     <input type="submit" hidden />
