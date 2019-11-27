e3b96882c0c749e659135eabc2318fbecc802f46
Santiago Enciso <whereispolaris@gmail.com>
2019-11-27 08:08:12 -0600

    Created handleSubmit function and added it to the form with an onSubmit event.

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -12,11 +12,14 @@ const Comments = React.createClass({
             </div>
         )
     },
+    handleSubmit(e) {
+        e.preventDefault();
+    },
     render() {
         return (
             <div className="comments">
                 {this.props.postComments.map(this.renderComment)}
-                <form ref="commentForm" className="comment-form">
+                <form ref="commentForm" className="comment-form" onSubmit={this.handeSumit}>
                     <input type="text" ref="author" placeholder="author" />
                     <input type="text" ref="comment" placeholder="comment" />
                     <input type="submit" hidden />
